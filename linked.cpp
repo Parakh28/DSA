4e2e240e41d853dae60ea5d8517a47c01c9a6a4d
PARAKH TYAGI <103480164+Parakh28@users.noreply.github.com>
2024-07-02 21:59:15 +0530

    Update linked.cpp

--- linked.cpp
+++ linked.cpp
@@ -183,32 +183,98 @@ Node* getintersection(Node* head1,Node* head2){
     return NULL;
     
 }
+
+void deletefromposend(Node* &head,int k){
+    
+    Node* pos1=head;
+    Node* pos2=head;
+    int count=k;
+    while(count--){
+        pos2=pos2->next;
+    }
+    
+    if(pos2==NULL){
+        Node* temp=head;
+        head=head->next;
+        free(temp);
+        return;
+    }
+    
+    
+    while(pos2->next!=NULL){
+        pos1=pos1->next;
+        pos2=pos2->next;
+    }
+    
+    
+    Node* temp=pos1->next;
+    
+    pos1->next=temp->next;
+    free(temp);
+}
+
+Node* mergelinklist(Node* head1,Node* head2){
+    
+    Node* dummayhead= new Node(-1);
+    Node* ptr1=head1;
+    Node* ptr2=head2;
+    Node* ptr3=dummayhead;
+    
+    while(ptr1!=NULL and ptr2!=NULL){
+        if(ptr1->val < ptr2->val){
+            ptr3->next=ptr1;
+            ptr1=ptr1->next;
+        }
+        else{
+            ptr3->next=ptr2;
+            ptr2=ptr2->next;
+        }
+        ptr3=ptr3->next;
+    }
+    if(ptr1){
+        ptr3->next=ptr1;
+    }
+    else{
+        ptr3->next=ptr2;
+    }
+    
+    return dummayhead->next;
+}
 int main(){
+    
     Linklist ll1;
     Linklist ll2;
+    Linklist ll3;
+    
 
     
         ll1.insertattail(1);
-        ll1.insertattail(2);
         ll1.insertattail(3);
-        ll1.insertattail(4);
         ll1.insertattail(5);
-        
+        ll2.insertattail(2);
+        ll2.insertattail(4);
         ll1.display();
-        
-        ll2.insertattail(6);
-        ll2.insertattail(7);
-        ll2.head->next->next=ll1.head->next->next->next;
-    
         ll2.display();
+        ll3.head=mergelinklist(ll1.head,ll2.head);
+        ll3.display();
+        
+        
+        //ll1.display();
+        //deletefromposend(ll1.head,5);
+        //ll1.display();
+        //ll2.insertattail(1);
+        //ll2.insertattail(2);*/
+        //l2.head->next->next=ll1.head->next->next->next;
+
+        //ll2.display();
     
-        Node* interaction = getintersection(ll1.head,ll2.head);
+       /*Node* interaction = getintersection(ll1.head,ll2.head);
         if(interaction){
             cout<<interaction->val<<endl;
         }
         else{
             cout<<"-1"<<endl;
-        }
+        }*/
     
     //ll1.display();
     //ll2.display();
